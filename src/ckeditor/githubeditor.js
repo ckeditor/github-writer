2cdba181cb1f35867d20de1fc30325568b37cd62
fredck <f.knabben@cksource.com>
2019-11-12 14:23:19 +0100

    Introduced the Heading feature.

--- src/ckeditor/githubeditor.js
+++ src/ckeditor/githubeditor.js
@@ -9,6 +9,9 @@ import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataproces
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 
@@ -20,9 +23,22 @@ export default class GitHubEditor extends DecoupledEditor {
 		super( initialData, {
 			plugins: [
 				Essentials, Paragraph,
+				Heading,
 				Bold, Italic
 			],
-			placeholder: 'Leave a comment'
+			placeholder: 'Leave a comment',
+			heading: {
+				options: [
+					{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+					{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+					{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+					{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+					{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+					{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+					{ model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+				]
+			}
+
 		} );
 
 		this.data.processor = new GFMDataProcessor();
