751dcc1555c75232e119410a9ad789810e5fbb14
fredck <f.knabben@cksource.com>
2019-11-21 20:29:08 +0100

    Introduced mentions support for emojis (:). Just unicode emojis supported for now.

--- src/ckeditor/githubmentionfeeds.js
+++ src/ckeditor/githubmentionfeeds.js
@@ -89,7 +89,53 @@ export default function getMentionFeedsConfig( urls ) {
 				return template.content.firstElementChild;
 			}
 		},
-		emoji: { cache: {} }
+		emoji: {
+			cache: {},
+			entryWorker: entryLiElement => {
+				/*
+				<li id="emoji-grinning" data-value=":grinning:" data-emoji-name="grinning" data-text="grinning smile happy">
+					<g-emoji>😀</g-emoji> grinning
+				</li>
+
+				// TODO: GH offers some non-unicod emojis as well, with the following format. This is not supported for now.
+				<li role="option" id="emoji-trollface" data-value=":trollface:" data-emoji-name="trollface" data-text="trollface">
+          			<img class="emoji emoji-result" height="20" width="20" align="absmiddle" alt=":trollface:"
+          				src="https://github.githubassets.com/images/icons/emoji/trollface.png">
+					trollface
+				</li>
+				*/
+
+				// Do nothing for non-unicode emojis.
+				if ( !entryLiElement.querySelector( 'g-emoji' ) ) {
+					return;
+				}
+
+				const text = entryLiElement.getAttribute( 'data-text' );
+
+				// The search key is a string in the format "id title_lowercase title_initials_lowercase".
+				const key =
+					text + ' ' +
+					text.match( /\b\w/g ).join( '' );	// All initials.
+
+				return {
+					key,
+					data: {
+						icon: entryLiElement.querySelector( 'g-emoji' ).textContent,
+						name: entryLiElement.getAttribute( 'data-emoji-name' )
+					}
+				};
+			},
+			entryRenderer: entry => {
+				const template = document.createElement( 'template' );
+				template.innerHTML = `
+					<div>
+						<g-emoji>${ entry.icon }</g-emoji> <small>${ entry.name }</small>
+					</div>
+				`;
+
+				return template.content.firstElementChild;
+			}
+		}
 	};
 
 	return [
@@ -116,15 +162,15 @@ export default function getMentionFeedsConfig( urls ) {
 
 		return new Promise( ( resolve, reject ) => {
 			// Try to take the results from the cache, if this query has already been made.
-			if ( db[type].cache[ query ] ) {
-				resolve( db[type].cache[ query ] );
+			if ( db[ type ].cache[ query ] ) {
+				resolve( db[ type ].cache[ query ] );
 				return;
 			}
 
 			getEntries( type )
 				.then( entries => {
 					// Cache the results.
-					const results = db[type].cache[ query ] = [];
+					const results = db[ type ].cache[ query ] = [];
 
 					// Got though all entries, searching for keys that include the query.
 					// Fill it up until we have 5 results.
@@ -148,18 +194,32 @@ export default function getMentionFeedsConfig( urls ) {
 				return;
 			}
 
-			downloadData( urls[ type ] )
+			downloadData( urls[ type ], [ 'issues', 'people' ].includes( type ) )
 				.then( data => {
 					if ( !data ) {
 						reject( new Error( 'Error when loading mentions from GitHub. No data returned.' ) );
 						return;
 					}
-					const entries = db[ type ].entries = [];
+
+					// Emojis don't come as JSON but HTML ul>li list. The worker expects the li elements.
+					if ( type == 'emoji' ) {
+						const root = document.createElement( 'template' );
+						root.innerHTML = data;
+						data = Array.from( root.content.firstElementChild.getElementsByTagName( 'li' ) );
+					}
+
 					const entryWorker = db[ type ].entryWorker;
 
-					data.forEach( dataEntry => {
-						entries.push( entryWorker( dataEntry ) );
-					} );
+					const entries = db[ type ].entries = data.reduce( ( entries, dataEntry ) => {
+						const entry = entryWorker( dataEntry );
+
+						// Workers can return falsy to ignore an entry.
+						if ( entry ) {
+							entries.push( entry );
+						}
+
+						return entries;
+					}, [] );
 
 					resolve( entries );
 				} )
@@ -167,12 +227,16 @@ export default function getMentionFeedsConfig( urls ) {
 		} );
 	}
 
-	function downloadData( url ) {
+	function downloadData( url, json ) {
 		return new Promise( ( resolve, reject ) => {
 			const xhr = new XMLHttpRequest();
 			xhr.open( 'GET', url, true );
-			xhr.responseType = 'json';
-			xhr.setRequestHeader( 'Accept', 'application/json' );
+
+			if ( json ) {
+				xhr.responseType = 'json';
+				xhr.setRequestHeader( 'Accept', 'application/json' );
+			}
+
 			xhr.setRequestHeader( 'X-Requested-With', 'XMLHttpRequest' );
 
 			xhr.addEventListener( 'error', () => reject( new Error( `Error loading mentions from $(url).` ) ) );
