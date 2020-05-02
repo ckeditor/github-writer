5616e10593d068cf30ec95db45ffaa51cf29c09a
fredck <f.knabben@cksource.com>
2020-05-03 00:16:44 +0200

    Fixed the priority of the code attribute so it'll not wrap other inline elements.

--- tests/unit/editors/ckeditorgithubeditor.js
+++ tests/unit/editors/ckeditorgithubeditor.js
@@ -6,6 +6,12 @@
 import CKEditorGitHubEditor from '../../../src/app/editors/ckeditorgithubeditor';
 import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor';
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+import BoldEditing from '@ckeditor/ckeditor5-basic-styles/src/bold/boldediting';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import ItalicEditing from '@ckeditor/ckeditor5-basic-styles/src/italic/italicediting';
+import StrikethroughEditing from '@ckeditor/ckeditor5-basic-styles/src/strikethrough/strikethroughediting';
+import CodeEditing from '@ckeditor/ckeditor5-basic-styles/src/code/codeediting';
+import { setData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
 
 describe( 'Editors', () => {
 	describe( 'CKEditorGitHubEditor', () => {
@@ -35,6 +41,23 @@ describe( 'Editors', () => {
 
 			editor.inspect();
 			expect( stub.callCount ).to.equals( 1 );
+		} );
+
+		it( 'should fix the priority of the code element', () => {
+			return CKEditorGitHubEditor.create( '',
+				{ plugins: [ Paragraph, BoldEditing, ItalicEditing, StrikethroughEditing, CodeEditing ] } )
+				.then( editor => {
+					setData( editor.model, '<paragraph>Test <$text bold="true" code="true">text</$text></paragraph>' );
+					expect( editor.getData() ).to.equals( 'Test **`text`**' );
+
+					setData( editor.model, '<paragraph>Test <$text italic="true" code="true">text</$text></paragraph>' );
+					expect( editor.getData() ).to.equals( 'Test _`text`_' );
+
+					setData( editor.model, '<paragraph>Test <$text strikethrough="true" code="true">text</$text></paragraph>' );
+					expect( editor.getData() ).to.equals( 'Test ~`text`~' );
+
+					return editor.destroy();
+				} );
 		} );
 	} );
 } );
